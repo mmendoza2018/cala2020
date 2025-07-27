e992e2c89f3b39e69e657c792422a4ec81f13f45
Miguel Mendoza <mendoza.ing1826@gmail.com>
2025-07-26 23:56:52 -0500

    adicon de validacion si no existen tablas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Models\General;
 use App\Models\PaymentMethod;
 use App\Models\SocialNetwork;
 use App\Models\Theme;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,19 +26,38 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $generalInfo = General::first();
-        $themes = Theme::first();
-        $attentionNumbers = AttentionNumber::where("status", 1)->get();
-        $socialNetworks = SocialNetwork::where("status", 1)->get();
-        $paymentMethods = PaymentMethod::where("status", 1)->get();
-        
+        $generalInfo = null;
+        $themes = null;
+        $attentionNumbers = collect();
+        $socialNetworks = collect();
+        $paymentMethods = collect();
+
+        if (Schema::hasTable('generals')) {
+            $generalInfo = \App\Models\General::first();
+        }
+
+        if (Schema::hasTable('themes')) {
+            $themes = \App\Models\Theme::first();
+        }
+
+        if (Schema::hasTable('attention_numbers')) {
+            $attentionNumbers = \App\Models\AttentionNumber::where("status", 1)->get();
+        }
+
+        if (Schema::hasTable('social_networks')) {
+            $socialNetworks = \App\Models\SocialNetwork::where("status", 1)->get();
+        }
+
+        if (Schema::hasTable('payment_methods')) {
+            $paymentMethods = \App\Models\PaymentMethod::where("status", 1)->get();
+        }
 
         View::share([
             'generalInfo' => $generalInfo,
             'themes' => $themes,
             'attentionNumbers' => $attentionNumbers,
             'socialNetworks' => $socialNetworks,
-            'paymentMethods' => $paymentMethods
+            'paymentMethods' => $paymentMethods,
         ]);
     }
 }
